702950ce16c0d93c87e0ddd1b658e3c35baf6994
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-06-27 13:50:04 +0300

    Add user menu to word_detector.py program

--- source2024/word_detector.py
+++ source2024/word_detector.py
@@ -42,54 +42,91 @@ def main():
             print(f'File "{user_input}" does not exist!')
     correct_labels = label_test_audio(test_audio_filepath)
 
-    print("Waiting for SVM predictions...")
+    while True:
+        print("\nPlease select an option:")
+        print("1. Predict using SVM")
+        print("2. Predict using MLP")
+        print("3. Predict using Least Squares")
+        print("4. Predict using RNN")
+        print("5. Exit")
 
-    # Predict using the SVM classifier
-    path = "../auxiliary2024/classifiers/svm_classifier.joblib"
-    svm_model = load_svm_classifier(path)
-    svm_predictions, sample_rate = predict_audio_labels(test_audio_filepath, svm_model)
-    # Find word Boundaries
-    svm_boundaries = find_word_boundaries(svm_predictions, sample_rate)
-    print(f'SVM classification accuracy is {accuracy_score(correct_labels, svm_predictions)}')
-    print(f'SVM boundaries are {svm_boundaries}')
+        option = input("Your selection: ")
 
-    # Predict using the MLP classifier
-    path = "../auxiliary2024/classifiers/mlp_classifier.joblib"
-    mlp_model = load_mlp_classifier(path)
-    mlp_predictions, sample_rate = predict_audio_labels(test_audio_filepath, mlp_model)
-    # Find word Boundaries
-    mlp_boundaries = find_word_boundaries(mlp_predictions, sample_rate)
-    print(f'MLP classification accuracy is {accuracy_score(correct_labels, mlp_predictions)}')
-    print(f'MLP boundaries are {mlp_boundaries}')
+        if option == "1":
+            print("Waiting for SVM predictions...")
 
-    # Predict using the Least Squares classifier
-    path = "../auxiliary2024/classifiers/theta_least_squares_classifier.joblib"
-    least_squares_theta = load_theta_least_squares_classifier(path)
-    least_squares_predictions, sample_rate = predict_audio_labels(test_audio_filepath, least_squares_theta)
-    # Find word Boundaries
-    least_squares_boundaries = find_word_boundaries(least_squares_predictions, sample_rate)
-    print(f'Least Squares classification accuracy is {accuracy_score(correct_labels, least_squares_predictions)}')
-    print(f'Least Squares boundaries are {least_squares_boundaries}')
+            # Predict using the SVM classifier
+            path = "../auxiliary2024/classifiers/svm_classifier.joblib"
+            svm_model = load_svm_classifier(path)
+            svm_predictions, sample_rate = predict_audio_labels(test_audio_filepath, svm_model)
 
-    # Predict using the RNN classifier
-    path = "../auxiliary2024/classifiers/rnn_classifier.keras"
-    rnn_model = load_rnn_model(path)
-    rnn_predictions, sample_rate = predict_audio_labels(test_audio_filepath, rnn_model)
-    # Find word Boundaries
-    rnn_boundaries = find_word_boundaries(rnn_predictions, sample_rate)
-    print(f'RNN classification accuracy is {accuracy_score(correct_labels, rnn_predictions)}')
-    print(f'RNN boundaries are {rnn_boundaries}')
+            # Find word Boundaries
+            svm_boundaries = find_word_boundaries(svm_predictions, sample_rate)
+            print(f'SVM classification accuracy is {accuracy_score(correct_labels, svm_predictions)}')
+            print(f'SVM boundaries are {svm_boundaries}')
 
-    # Play the audio with the boundaries
-    print("Playing audio with boundaries...")
-    print("SVM boundaries:")
-    play_audio_boundaries(test_audio_filepath, svm_boundaries)
-    print("MLP boundaries:")
-    play_audio_boundaries(test_audio_filepath, mlp_boundaries)
-    print("Least Squares boundaries:")
-    play_audio_boundaries(test_audio_filepath, least_squares_boundaries)
-    print("RNN boundaries:")
-    play_audio_boundaries(test_audio_filepath, rnn_boundaries)
+            # Play the audio with the boundaries
+            print("Playing audio with boundaries...")
+            print("SVM boundaries:")
+            play_audio_boundaries(test_audio_filepath, svm_boundaries)
+
+        elif option == "2":
+            print("Waiting for MLP predictions...")
+
+            # Predict using the MLP classifier
+            path = "../auxiliary2024/classifiers/mlp_classifier.joblib"
+            mlp_model = load_mlp_classifier(path)
+            mlp_predictions, sample_rate = predict_audio_labels(test_audio_filepath, mlp_model)
+
+            # Find word Boundaries
+            mlp_boundaries = find_word_boundaries(mlp_predictions, sample_rate)
+            print(f'MLP classification accuracy is {accuracy_score(correct_labels, mlp_predictions)}')
+            print(f'MLP boundaries are {mlp_boundaries}')
+
+            # Play the audio with the boundaries
+            print("Playing audio with boundaries...")
+            print("MLP boundaries:")
+            play_audio_boundaries(test_audio_filepath, mlp_boundaries)
+
+        elif option == "3":
+            print("Waiting for Least Squares predictions...")
+
+            # Predict using the Least Squares classifier
+            path = "../auxiliary2024/classifiers/theta_least_squares_classifier.joblib"
+            least_squares_theta = load_theta_least_squares_classifier(path)
+            least_squares_predictions, sample_rate = predict_audio_labels(test_audio_filepath, least_squares_theta)
+
+            # Find word Boundaries
+            least_squares_boundaries = find_word_boundaries(least_squares_predictions, sample_rate)
+            print(f'Least Squares classification accuracy is {accuracy_score(correct_labels, least_squares_predictions)}')
+            print(f'Least Squares boundaries are {least_squares_boundaries}')
+
+            # Play the audio with the boundaries
+            print("Playing audio with boundaries...")
+            print("Least Squares boundaries:")
+            play_audio_boundaries(test_audio_filepath, least_squares_boundaries)
+
+        elif option == "4":
+            print("Waiting for RNN predictions...")
+
+            # Predict using the RNN classifier
+            path = "../auxiliary2024/classifiers/rnn_classifier.keras"
+            rnn_model = load_rnn_model(path)
+            rnn_predictions, sample_rate = predict_audio_labels(test_audio_filepath, rnn_model)
+
+            # Find word Boundaries
+            rnn_boundaries = find_word_boundaries(rnn_predictions, sample_rate)
+            print(f'RNN classification accuracy is {accuracy_score(correct_labels, rnn_predictions)}')
+            print(f'RNN boundaries are {rnn_boundaries}')
+
+            # Play the audio with the boundaries
+            print("Playing audio with boundaries...")
+            print("RNN boundaries:")
+            play_audio_boundaries(test_audio_filepath, rnn_boundaries)
+
+        elif option == "5":
+            print("Exiting program...")
+            break
 
 
 if __name__ == '__main__':
